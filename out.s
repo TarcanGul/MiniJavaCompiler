774324d714af293d95f9e0bf15a0c71bfa2c8fd1
TarcanGul <tgul@purdue.edu>
2019-11-15 18:57:08 -0500

    out

--- out.s
+++ out.s
@@ -43,7 +43,7 @@ mov r0, #2
 mul r0, r0, r1
 ldr r4, =_t0
 str r0, [r4]
-ldr r4, =(null)
+ldr r4, =_t0
 ldr r1, [r4]
 mov r0, #2
 add r0, r0, r1
@@ -58,13 +58,13 @@ mov r0, #5
 sub r0, r1, r0
 ldr r4, =_t2
 str r0, [r4]
-ldr r4, =(null)
+ldr r4, =_t2
 ldr r1, [r4]
 mov r0, #16
 mul r0, r0, r1
 ldr r4, =_t3
 str r0, [r4]
-ldr r4, =(null)
+ldr r4, =_t3
 ldr r0, [r4]
 mov r4, r0
 str r4, [fp, #-20]
@@ -74,12 +74,15 @@ ldr r0, =println_int_format
 bl printf
 ldr r0, [fp, #-8]
 cmp r0, #1
+bne _false0
 _true0:
 ldr r0, =println_str_format
 ldr r1, =bool_true
+bl printf
 _false0:
 ldr r0, =println_str_format
 ldr r1, =bool_false
+bl printf
 _endif0:
 mov sp, fp
 pop {fp, pc}
