968b5175d573e9ab92d1545a2d26e8452f3601ed
TarcanGul <tgul@purdue.edu>
2019-11-15 22:26:51 -0500

    out while might work?

--- out.s
+++ out.s
@@ -218,13 +218,13 @@ _endif2:
 ldr r0, [fp, #-8]
 mov r1, r0
 ldr r0, =#0
-cmp r0, r1
-bgt _false4
-_true4:
-mov r0, #1
-b _endif4
+cmp r1, r0
+bgt _true4
 _false4:
 mov r0, #0
+b _endif4
+_true4:
+mov r0, #1
 b _endif4
 _endif4:
 ldr r4, =_t10
@@ -267,13 +267,13 @@ str r4, [fp, #-24]
 ldr r0, [fp, #-24]
 mov r1, r0
 ldr r0, =#0
-cmp r0, r1
-bgt _false5
-_true5:
-mov r0, #1
-b _endif5
+cmp r1, r0
+bgt _true5
 _false5:
 mov r0, #0
+b _endif5
+_true5:
+mov r0, #1
 b _endif5
 _endif5:
 ldr r4, =_t13
